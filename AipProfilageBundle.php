fdbc3f604c0070631de03d498de0161cf0dee209
MaalejWiem <maalej.wiem@gmail.com>
2014-03-19 17:25:53 +0100

    Update AipProfilageBundle.php

--- AipProfilageBundle.php
+++ AipProfilageBundle.php
@@ -16,21 +16,14 @@ use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 class AipProfilageBundle extends PluginBundle implements ConfigurationProviderInterface
 
 {
-    public function getConfiguration($environements)
+     public function hasMigrations()
     {
-        $config= new ConfigurationBuilder();
-		return $config->addRoutingResource(__DIR__.'/Resources/config/routing.yml',null,'/');
+        return false;
     }
-	 public function suggestConfigurationFor(Bundle $bundle, $environment)
+    
+    public function getRequiredFixturesDirectory()
     {
-        if ($bundle instanceof MyThirdPartyBundle) {
-            $config = new ConfigurationBuilder();
-        
-            // ajout d'un fichier de routing valable pour tous les environnements
-            $config->addRoutingResource(__DIR__.'/Resources/config/routing.yml',null,'/');
-         
-            return $config;
-        }
+        return 'DataFixtures';
     }
 }
 
